697821d2a1ac64fbde97d9bdd9ea5a1ed0b94be7
webbdevv <kyle.xu4@gmail.com>
2021-05-26 12:13:27 -0700

    Finished REST routes

--- Routes_and_Controllers/app/controllers/users_controller.rb
+++ Routes_and_Controllers/app/controllers/users_controller.rb
@@ -2,13 +2,28 @@
 class UsersController < ApplicationController
 
     def create
-
-    end
-    def update
+        # user = User.create!(params.require(:user).permit(:email, :name))
+        # str = "12ajsdlfjasdflk"
+        # replace the `user_attributes_here` with the actual attribute keys
+        # redirect_to '/users'
+        # redirect_to action: 'index', user: str
+        # self.index
         
+          user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        u = User.find(params[:id])
+        u.update(name: params[:name], email: params[:email])
     end
     def destroy
-        
+        u = User.find(params[:id])
+        u.delete
     end
     def edit
         
@@ -17,10 +32,10 @@ class UsersController < ApplicationController
         
     end
     def show
-        
+        render json: User.find(params[:id])
     end
     def index
-        
+        render json: User.all
     end
 
 end
