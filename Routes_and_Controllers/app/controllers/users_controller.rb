953abb227cbbedafb1fead0a8fc039e4bc5cc5cd
Donnie Wombough <donniewomboughjunior@yahoo.com>
2021-05-26 14:12:08 -0400

    init

---
+++ Routes_and_Controllers/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+
+class UsersController < ApplicationController
+
+    def create
+
+    end
+    def update
+        
+    end
+    def destroy
+        
+    end
+    def edit
+        
+    end
+    def new
+        
+    end
+    def show
+        
+    end
+    def index
+        
+    end
+
+end
